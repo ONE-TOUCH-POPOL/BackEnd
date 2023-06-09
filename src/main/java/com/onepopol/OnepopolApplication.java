46076b8952bb73d74a4602a1d169e1a4746c4a47
slbin-park <smpts00@naver.com>
2023-06-09 13:57:16 +0900

    First Commit

---
+++ src/main/java/com/onepopol/OnepopolApplication.java
@@ -0,0 +1,13 @@
+package com.onepopol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepopolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepopolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onepopol/OnepopolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onepopol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepopolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
