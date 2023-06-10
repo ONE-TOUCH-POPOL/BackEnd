4cbef974e27214b04c60f4c64df770fd639b465e
deerKBS <deer980119@gmail.com>
2023-06-10 22:01:05 +0900

    refactor: 회원가입, 로그인, 토큰 유효성 확인, 토큰 재발급, 로그아웃 기능 추가 - 회원가입 기능 추가 - spring security, jwt, redis를 사용해 로그인 시 access token, refresh token 발급 - 사용자 id redis에 저장 기능 추가 - access token 유효성 검사 기능 추가 - 토큰 재발급 기능 추가 - 로그아웃 기능 추가

---
+++ src/main/java/com/onepopol/member/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.onepopol.member.security;
+
+import com.onepopol.member.domain.User;
+import com.onepopol.member.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsImpl loadUserByUsername(String email) throws UsernameNotFoundException {
+        User findUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find user with this email. -> " + email));
+
+        if(findUser != null){
+            UserDetailsImpl userDetails = new UserDetailsImpl(findUser);
+            return  userDetails;
+        }
+
+        return null;
+    }
+}
