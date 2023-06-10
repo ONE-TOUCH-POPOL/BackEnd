545475401c31e1f9745ef9736c6c3ff516f7c964
deerKBS <deer980119@gmail.com>
2023-06-10 21:56:46 +0900

    refactor: 기존 회원가입, 로그인 관련 파일 삭제 - spring security만 적용된 파일들 삭제

--- src/main/java/com/onepopol/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onepopol.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-    public Member findByUsername(String loginId);
-}
--- src/main/java/com/onepopol/member/MemberService.java
+++
@@ -1,5 +0,0 @@
-package com.onepopol.member;
-
-public interface MemberService {
-    public int registerMember(Member member);
-}
