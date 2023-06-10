315875c70bb4be0c04444807b466f0bdb3793e98
deerKBS <deer980119@gmail.com>
2023-06-11 01:54:52 +0900

    feat: email, password valid 조건 추가 - 각 변수에 @NotBlank와 @Pattern 추가

--- src/main/java/com/onepopol/member/repository/entity/Member.java
+++ src/main/java/com/onepopol/member/repository/entity/Member.java
@@ -19,8 +19,12 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+//    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+//    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
     private String email; // Principal
 
+//    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+//    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password; // Credential
 
     @Enumerated(EnumType.STRING)
