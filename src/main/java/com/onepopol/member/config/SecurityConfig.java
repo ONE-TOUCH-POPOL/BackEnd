986cc4a3b4af3150ebbfce4b5c8a630de8953ada
deerKBS <deer980119@gmail.com>
2023-06-10 15:41:30 +0900

    refactor: SecurityConfig 수정 - WebSecurityConfigurerAdapter 더이상 사용되지 않아서 제거하고 SecurityFilterChan을 리턴하는 메소드로 변경

--- src/main/java/com/onepopol/member/config/SecurityConfig.java
+++ src/main/java/com/onepopol/member/config/SecurityConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록 된다.
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
@@ -17,11 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable(); //csrf 비활성화
         http.authorizeRequests()
-                .antMatchers("/user/**").authenticated() // 인증만 되면 들어갈 수 있는 주소
+//                .antMatchers("/user/**").authenticated() // 인증만 되면 들어갈 수 있는 주소
 //                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
 //                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll() // 인증받지 않아도 모두 갈 수 있도록 설정
@@ -30,5 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
                 .defaultSuccessUrl("/");
+        return http.build();
     }
 }
