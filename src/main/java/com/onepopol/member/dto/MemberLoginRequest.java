b780e10c69652ecd513943d635b4e602f5adfeea
deerKBS <deer980119@gmail.com>
2023-06-14 00:54:18 +0900

    feat: 로그인 시 유효성 체크 기능 추가 - 유효성 체크를 위한 코드 추가

--- src/main/java/com/onepopol/member/dto/MemberLoginRequest.java
+++ src/main/java/com/onepopol/member/dto/MemberLoginRequest.java
@@ -5,10 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberLoginRequest {
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[A-Z0-9.%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$", message = "이메일 형식이 올바르지 않습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d).{6,}$", message = "비밀번호 형식이 올바르지 않습니다.")
     private String password;
 
     @Builder
