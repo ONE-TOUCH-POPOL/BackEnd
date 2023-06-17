c47f7f2425ab14d26929e4b8cdde3cdb9407e997
slbin-park <smpts00@naver.com>
2023-06-17 20:55:42 +0900

    refactor: Credentials 옵션 제거

--- src/main/java/com/onepopol/config/WebConfig.java
+++ src/main/java/com/onepopol/config/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedOrigins("*")  // 허용되는 도메인. "*"은 모든 도메인 허용을 의미합니다.
                 .allowedMethods("*")  // 허용되는 HTTP 메소드. "*"은 모든 메소드를 허용합니다.
-                .allowedHeaders("*")  // 허용되는 HTTP 헤더. "*"은 모든 헤더를 허용합니다.
-                .allowCredentials(true); // 쿠키, 세션 정보도 허용
+                .allowedHeaders("*");  // 허용되는 HTTP 헤더. "*"은 모든 헤더를 허용합니다.
     }
 }
