f8fd8172c598df2f796f5022921e2bfc4610a2aa
slbin-park <smpts00@naver.com>
2023-06-17 22:17:39 +0900

    .

--- src/main/java/com/onepopol/config/WebConfig.java
+++ src/main/java/com/onepopol/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
-                .allowedOriginPatterns("*")  // 허용되는 도메인. "*"은 모든 도메인 허용을 의미합니다.
+//                .allowedOriginPatterns("*")  // 허용되는 도메인. "*"은 모든 도메인 허용을 의미합니다.
+                .allowedOrigins("http://127.0.0.1:3000")
                 .allowedMethods("*")  // 허용되는 HTTP 메소드. "*"은 모든 메소드를 허용합니다.
                 .allowedHeaders("*")  // 허용되는 HTTP 헤더. "*"은 모든 헤더를 허용합니다.
                 .allowCredentials(true) // 쿠키, 세션 정보도 허용
