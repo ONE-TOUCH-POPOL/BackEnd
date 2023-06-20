e44b08bd4286668417d1ac3518ffa05d7c7087b3
deerKBS <deer980119@gmail.com>
2023-06-20 22:42:18 +0900

    feat: 유효성 관련 설정 추가 - language 와 sourceCode valid 유효성 설정 추가

--- src/main/java/com/onepopol/codeFormat/dto/CodeFormatRequest.java
+++ src/main/java/com/onepopol/codeFormat/dto/CodeFormatRequest.java
@@ -5,10 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CodeFormatRequest {
+    @NotBlank
     String language;
+    @NotBlank
     String sourceCode;
 }
