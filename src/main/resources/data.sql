0cfe2cda3cf8ac23984ae5d4d5b73e527b71ff11
slbin-park <smpts00@naver.com>
2023-06-20 18:49:22 +0900

    chore: 뱃지 , 카테고리 임시 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
+-- 메인 카테고리 생성
 insert into main_category(code_name)
 VALUES ("IT");
 insert into main_category(code_name)
 VALUES ("운동");
+-- 서브 카테고리 생성
 insert into sub_category(code_name,main_code)
 VALUES ("스프링부트",1);
 insert into sub_category(code_name,main_code)
@@ -14,3 +16,11 @@ insert into sub_category(code_name,main_code)
 VALUES ("헬스",2);
 insert into sub_category(code_name,main_code)
 VALUES ("허리",2);
+-- 뱃지 생성
+insert into badge_category(badgeName)
+VALUES ("GPT");
+insert into badge_category(badgeName)
+VALUES ("정리");
+insert into badge_category(badgeName)
+VALUES ("내가쓴글");
+
