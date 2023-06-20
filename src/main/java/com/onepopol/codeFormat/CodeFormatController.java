c73e6a7fa9764c72eb005e92da7ccc7419f2868e
deerKBS <deer980119@gmail.com>
2023-06-20 22:40:28 +0900

    feat: 유효성 확인 기능 추가 및 에러 처리 추가 - requestBody 유효성 확인 기능 추가 - 포매팅 기능 에러 처리 추가

--- src/main/java/com/onepopol/codeFormat/CodeFormatController.java
+++ src/main/java/com/onepopol/codeFormat/CodeFormatController.java
@@ -2,12 +2,18 @@ package com.onepopol.codeFormat;
 
 import com.onepopol.codeFormat.dto.CodeFormatRequest;
 import com.onepopol.codeFormat.service.CodeFormattingService;
+import com.onepopol.config.BaseException;
+import com.onepopol.config.ValidationException;
 import com.onepopol.utils.ApiResult;
 import com.onepopol.utils.Apiutils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +24,22 @@ public class CodeFormatController {
     private final CodeFormattingService codeFormattingService;
 
     @PostMapping()
-    public ApiResult<?> formatCode(@RequestBody CodeFormatRequest request) throws IOException {
-        String language = request.getLanguage();
-        String sourceCode = request.getSourceCode();
+    public ApiResult<?> formatCode(@Valid @RequestBody CodeFormatRequest request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // 유효성 검사 실패 시 처리
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            throw new ValidationException(fieldErrors);
+        }
+        try {
+            String language = request.getLanguage();
+            String sourceCode = request.getSourceCode();
 
-        String result = codeFormattingService.formatCode(language,sourceCode);
+            String result = codeFormattingService.formatCode(language, sourceCode);
 
-        return Apiutils.success(result);
+            return Apiutils.success(result);
+        }catch(BaseException e){
+            throw new BaseException(e.getApiError());
+        }
     }
 
     // demo version
